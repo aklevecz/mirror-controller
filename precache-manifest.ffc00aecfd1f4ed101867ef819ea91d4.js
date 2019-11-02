1ff90f6e3e3b7895ace664119e96ba93054a1ec9
traysimay <traysimay@rumormill.fyi>
2019-11-02 00:09:36 -0700

    Updates

---
+++ precache-manifest.ffc00aecfd1f4ed101867ef819ea91d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c97641a612cf5d4ba1edf5a5b31b410",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c892e83d98cb3b6bf466",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0bb6c4dc28a7b99bb18b",
+    "url": "/static/js/2.0dc12ea6.chunk.js"
+  },
+  {
+    "revision": "c892e83d98cb3b6bf466",
+    "url": "/static/js/main.b33e03a4.chunk.js"
+  },
+  {
+    "revision": "8da1442a48880c4fc15d",
+    "url": "/static/js/runtime-main.fb54b558.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mirror-controller/precache-manifest.6f4182c93eca2d839ea290cccf1286c3.js"
+  "/precache-manifest.ffc00aecfd1f4ed101867ef819ea91d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mirror-controller/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
