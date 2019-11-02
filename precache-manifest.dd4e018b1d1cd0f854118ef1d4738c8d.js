aadb10434fbb8821bdab4d584f4daf9937359747
traysimay <traysimay@rumormill.fyi>
2019-11-01 23:42:06 -0700

    Updates

---
+++ precache-manifest.dd4e018b1d1cd0f854118ef1d4738c8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84a36c4f56ceb70e62f30dbfc52acee",
+    "url": "/mirror-controller/index.html"
+  },
+  {
+    "revision": "5e4472523a93dfc6d971",
+    "url": "/mirror-controller/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "9a5b08e68f88edb378cf",
+    "url": "/mirror-controller/static/js/2.4804e2f0.chunk.js"
+  },
+  {
+    "revision": "5e4472523a93dfc6d971",
+    "url": "/mirror-controller/static/js/main.e38e6789.chunk.js"
+  },
+  {
+    "revision": "d0c104974a96fd480209",
+    "url": "/mirror-controller/static/js/runtime-main.0bfabd63.js"
+  }
+]);
